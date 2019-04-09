acc27ac15a7de1eccc3164d5e2e939d4ea115089
sami-alajrami <sami@praqma.com>
2019-04-09 10:21:58 +0200

    bringing prod cluster uptodate with Dev cluster

--- .circleci/store_cluster_certs.sh
+++ .circleci/store_cluster_certs.sh
@@ -32,6 +32,7 @@ echo $(terraform output ${prefix}_client_certificate) | base64 -d > keys/${prefi
 echo $(terraform output ${prefix}_client_key) | base64 -d > keys/${prefix}_client_key.key
 echo $(terraform output ${prefix}_ca_certificate) | base64 -d > keys/${prefix}_cluster_ca.crt
 echo $(terraform output ${prefix}_endpoint) > endpoint.txt
+echo $(terraform output ${prefix}_ambassador_ip) > static_ip.txt
 
 # push secrets to GCS and cleanup the local file system
 if [[ -r keys/${prefix}_client_certificate.crt ]] && [[ -r keys/${prefix}_client_key.key ]] && [[ -r keys/${prefix}_cluster_ca.crt ]];then
@@ -42,7 +43,7 @@ if [[ -r keys/${prefix}_client_certificate.crt ]] && [[ -r keys/${prefix}_client
     exit 1
   else
     echo "Cleaning up local file system ... "
-    rm -fr keys && echo "Keys cleanup complete." || echo "Something went wrong during keys cleanup."
+    #rm -fr keys && echo "Keys cleanup complete." || echo "Something went wrong during keys cleanup."
   fi
 
 else
--- prod-cluster.tf
+++ prod-cluster.tf
@@ -1,6 +1,12 @@
-variable "project_name" {
+
+variable "project_id" {
   description = "Google Cloud project ID."
-  default     = "pi-ostelco-prod"
+  default = "pi-ostelco-prod"
+}
+
+variable "regional" {
+  description = "whether the cluster should be created in multiple zones or not."
+  default = false
 }
 
 variable "cluster_region" {
@@ -8,9 +14,9 @@ variable "cluster_region" {
   description = "The region where the cluster will be created."
 }
 
-variable "cluster_zone" {
-  default     = "europe-west1-c"
-  description = "The zone where the cluster will be created."
+variable "cluster_zones" {
+  default     = ["europe-west1-c"]
+  description = "The zone(s) where the cluster will be created."
 }
 
 variable "cluster_admin_password" {
@@ -18,67 +24,91 @@ variable "cluster_admin_password" {
 }
 
 # Configure the Google Cloud provider
-provider "google" {
-  project = "${var.project_name}"
+provider "google-beta" {
+  project = "${var.project_id}"
   region  = "${var.cluster_region}"
 }
 
 module "gke" {
   source              = "github.com/ostelco/ostelco-terraform-modules//terraform-google-gke-cluster"
-  cluster_password    = "${var.cluster_admin_password}"
-  cluster_name        = "pi-prod"
-  cluster_description = "Production cluster for Ostelco Pi."
-  cluster_version     = "1.10.6-gke.2"
-  cluster_zone        = "${var.cluster_zone}"
+  project_id            = "${var.project_id}"
+  cluster_password      = "${var.cluster_admin_password}"
+  cluster_name          = "pi-prod"
+  cluster_description   = "Production cluster for Ostelco Pi."
+  cluster_version       = "1.11.8-gke.4"
+  cluster_zones         = "${var.cluster_zones}"
+  regional              = "${var.regional}"
 
-  # the line below makes the cluster multizone (regional)
-  #cluster_additional_zones = ["europe-west1-b"]
 }
 
-module "pool1" {                                                                                    
-  source         = "github.com/ostelco/ostelco-terraform-modules//terraform-google-gke-node-pool"   
-  cluster_name   = "${module.gke.cluster_name}"                                                     
-  node_pool_zone = "${module.gke.cluster_zone}"                                                     
-                                                                                                    
-  node_pool_name         = "new-small-nodes-pool"                                                   
-  pool_min_node_count    = "1"                                                                      
-  pool_max_node_count    = "4"                                                                      
-  node_tags              = ["prod"]                                                                 
-                                                                                                    
-  # oauth_scopes define what Google API nodes in the pool have access to.                           
-  # list of APIs can be found here: https://developers.google.com/identity/protocols/googlescopes   
-  oauth_scopes = [                                                                                  
-      "https://www.googleapis.com/auth/compute",                                                    
-      "https://www.googleapis.com/auth/devstorage.read_write",                                      
-      "https://www.googleapis.com/auth/logging.write",                                              
-      "https://www.googleapis.com/auth/monitoring",                                                 
-      "https://www.googleapis.com/auth/service.management",                                         
-      "https://www.googleapis.com/auth/pubsub",                                                     
-      "https://www.googleapis.com/auth/datastore",                                                  
-      "https://www.googleapis.com/auth/bigquery",                                                   
-      "https://www.googleapis.com/auth/sqlservice.admin",                                           
-      "https://www.googleapis.com/auth/ndev.clouddns.readwrite",                                    
-    ]                                                                                               
-                                                                                                    
-  node_labels = {                                                                                
-    "env"         = "prod"                                                                       
-    "machineType" = "n1-standard-1"                                                              
-  }                    
+module "high-mem" {
+  source         = "github.com/ostelco/ostelco-terraform-modules//terraform-google-gke-node-pool"
+  project_id     = "${var.project_id}"
+  regional       = "${var.regional}"
+  cluster_name   = "${module.gke.cluster_name}" # creates implicit dependency
+  cluster_region = "${var.cluster_region}"
+  node_pool_zone = "${var.cluster_zones[0]}"
+
+  node_pool_name = "highmem-pool"
+  pool_min_node_count    = "2"
+  initial_node_pool_size = "3"
+  pool_max_node_count    = "6"
+  node_tags              = ["high-mem"]
+  auto_upgrade           = true
+
+  node_labels = {
+    "env"         = "high-mem"
+    "machineType" = "n1-highmem-2"
+  }
+  
+  # oauth_scopes define what Google API nodes in the pool have access to.
+  # list of APIs can be found here: https://developers.google.com/identity/protocols/googlescopes
+  oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management",
+      "https://www.googleapis.com/auth/pubsub",
+      "https://www.googleapis.com/auth/datastore",
+      "https://www.googleapis.com/auth/bigquery",
+      "https://www.googleapis.com/auth/sqlservice.admin",
+      "https://www.googleapis.com/auth/ndev.clouddns.readwrite", 
+      "https://www.googleapis.com/auth/servicecontrol",
+    ]
+
+
 }
+
+resource "google_compute_address" "static_ambassador_ip" {
+  provider = "google-beta"
+  name = "ambassador-static-ip"
+  description = "A static external IP for prod Ambassador LB"
+}
+
+output "prod_cluster_ambassador_ip" {
+  sensitive = true
+  value = "${google_compute_address.static_ambassador_ip.address}"
+}
+
 
 output "prod_cluster_endpoint" {
+  sensitive = true
   value = "${module.gke.cluster_endpoint}"
 }
 
 output "prod_cluster_client_certificate" {
+  sensitive = true
   value = "${module.gke.cluster_client_certificate}"
 }
 
 output "prod_cluster_client_key" {
+  sensitive = true
   value = "${module.gke.cluster_client_key}"
 }
 
 output "prod_cluster_ca_certificate" {
+  sensitive = true
   value = "${module.gke.cluster_ca_certificate}"
 }
 
